f3c0402ec48fe0afe3ed5fee830f3ae77d050859
Arun Muraleedharan <arunmuraliam.707@gmail.com>
2023-02-09 23:27:31 +0530

    User, Admin authentication and autherization completed

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB");
+        //await mongoose.connect('mongodb://localhost:27017/test')
+    } catch (error) {
+        throw error
+    }
+};
+
+//middleware
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected");
+})
+
+
+app.listen(8080, () => {
+    connect();
+    console.log("Connected to backend.");
+})
